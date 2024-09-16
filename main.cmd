76a9a8c28279e9ac9172fd440872b0bf945ad544
Sofakin <54062133+sof4k1n@users.noreply.github.com>
2024-09-16 22:33:16 +0500

    Delete main.cmd

--- main.cmd
+++
@@ -1,7 +0,0 @@
-
-@echo off
-rem This script was created by Nuitka to execute 'main.exe' with Python DLL being found.
-set PATH=C:\Users\sofakin\AppData\Local\Programs\Python\PYTHON~1;%PATH%
-set PYTHONHOME=C:\Users\sofakin\AppData\Local\Programs\Python\Python312
-set NUITKA_PYTHONPATH=C:\Users\sofakin\Documents\Blum-ticket-farm;C:\Users\sofakin\AppData\Local\Programs\Python\Python312\DLLs;C:\Users\sofakin\AppData\Local\Programs\Python\Python312\Lib;C:\Users\sofakin\AppData\Local\Programs\Python\Python312;C:\Users\sofakin\AppData\Local\Programs\Python\Python312\Lib\site-packages
-"%~dp0main.exe" %*

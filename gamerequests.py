54c47f6b68ac07c36b3646cb45afb406f944eb98
Sofakin <54062133+sof4k1n@users.noreply.github.com>
2024-09-17 10:27:23 +0500

    Update gamerequests.py

--- gamerequests.py
+++ gamerequests.py
@@ -23,6 +23,7 @@ def claim(urlClaim, headers, payload, jwt):
 
 def auth(urlAuth, headers, token):
     r = requests.post(urlAuth, headers=headers, json=token)
-    r.encoding = "RFC 7932"
-    jwt = json(r.content).get("token").get("access")
+    r = r.content
+    jr = r.encoding = "RFC 7932"
+    jwt = json.loads(jr).get("token").get("access")
     return f"Bearer {jwt}"
